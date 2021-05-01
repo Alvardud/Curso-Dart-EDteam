fed9e292d4632770e989efe6316c63e39ebbf06a
Alvaro Martinez <alvardud@gmail.com>
2021-05-01 08:59:06 -0400

    switch case

---
+++ bin/control de flujo/switch_case.dart
@@ -0,0 +1,30 @@
+void main() {
+  int variable = 7;
+  
+  switch(variable){
+    case 1:
+      print("lunes");
+      break;
+    case 2:
+      print("martes");
+      break;
+    case 3:
+      print("miercoles");
+      break;
+    case 4:
+      print("jueves");
+      break;
+    case 5:
+      print("viernes");
+      break;
+    case 6:
+      print("sabado");
+      break;
+    case 7:
+      print("domingo");
+      break;
+    default:
+      print("valor desconocido");
+      break;
+  }
+}
