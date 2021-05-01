b6c685631acb8e466e7fb00c51fbe99f03a562d2
Alvaro Martinez <alvardud@gmail.com>
2021-05-01 15:13:08 -0400

    for for each

---
+++ bin/control de flujo/ciclo_for_for_each.dart
@@ -0,0 +1,12 @@
+void main() {
+  for(int picker = 0; picker < 5; picker++){
+    //print("El valor de picker es: $picker");
+  }
+  
+  final lista = ["alvaro",'ariel','martinez','mancilla'];
+  
+  lista.forEach((elemento){
+    print(elemento);
+  });
+  
+}
