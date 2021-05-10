fee785357c322addb51e5476a9fc5a2831d2b1e1
Alvaro Martinez <alvardud@gmail.com>
2021-05-10 18:16:35 -0400

    clases

---
+++ bin/programacion orientada a objetos/clases.dart
@@ -0,0 +1,14 @@
+void main() {
+  int numero = 10;
+  String cadena = "numero";
+  bool verdad = true;
+  print(cadena+numero.toString()+verdad.toString()); 
+}
+
+class Objeto {
+  // propiedades
+  // ej. color, tamaño, etc...
+
+  // metodos
+  // ej. mostrar, crear, etc...
+}
