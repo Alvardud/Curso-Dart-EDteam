650c68f10fda9b9abfa5fd18f26466448e672cb9
Alvaro Martinez <alvardud@gmail.com>
2021-05-25 19:05:19 -0400

    elementos nulos

---
+++ bin/seguridad nula/elementos_nulos.dart
@@ -0,0 +1,14 @@
+void main() {
+   int numero = 10;
+   print(numero);
+  
+  print(numero+5);
+  
+  String? palabra;
+  print(palabra);
+  
+  int? decena = 10;
+  print(decena);
+  decena = null;
+  print(decena);
+}
